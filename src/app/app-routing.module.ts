17a7b385f113b896d8f660b3e2c02a74a16da772
Takwa <maaroufitakwa96@gmail.com>
2021-11-11 07:43:41 +0100

    add tache module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'task', loadChildren: () => import('./tache/tache.module').then(m => m.TacheModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
