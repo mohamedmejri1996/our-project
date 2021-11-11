152651258cbef99d6f30b72d8706ee0fe720b0da
Aya Jaddi <jaddiaya123@gmail.com>
2021-11-11 11:16:13 +0100

    add module employees

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'employees', loadChildren: () => import('./employees/employees.module').then(m => m.EmployeesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
