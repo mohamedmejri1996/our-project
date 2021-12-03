44fc5eabc92f4b75d36873a5e12c4795ae007507
mohamedmejri1996 <mohamedmejri1996@gmail.com>
2021-12-03 03:42:16 +0100

    ajouter un fichier ts pour les chantier et afficher tous et une seule

--- src/app/chantier/chantier-routing.module.ts
+++ src/app/chantier/chantier-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChantierComponent } from './chantier.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [{ path: '', component: ChantierComponent }];
+const routes: Routes = [{ path: '', component: ChantierComponent },
+{path:'details/:chantierid',component: DetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
