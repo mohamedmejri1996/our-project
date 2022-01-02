fe9a4955083b9a73c27207f479ec0834452ac6c8
jaafer <jarrayjaafer@gmail.com>
2022-01-02 22:00:04 +0000

    fix erreur map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 
-const routes: Routes = [{ path: 'task', loadChildren: () => import('./tache/tache.module').then(m => m.TacheModule) }];
+const routes: Routes = [{ path: 'task', loadChildren: () => import('./tache/tache.module').then(m => m.TacheModule) } ];
 
 
 const routes1: Routes = [{ path: 'employees', loadChildren: () => import('./employees/employees.module').then(m => m.EmployeesModule) }];
 
 const routes2: Routes = [{ path: 'chantier', loadChildren: () => import('./chantier/chantier.module').then(m => m.ChantierModule) }];
 
-
+const routes3: Routes = [{ path: 'map', loadChildren: () => import('./map/map.module').then(m => m.MapModule) }]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),RouterModule.forRoot(routes1),RouterModule.forRoot(routes2)],
+  imports: [RouterModule.forRoot(routes),RouterModule.forRoot(routes1),RouterModule.forRoot(routes2),RouterModule.forRoot(routes3)],
   exports: [RouterModule],
 
 })
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,54 +1,9 @@
 import { Component } from '@angular/core';
-import { latLng, tileLayer } from 'leaflet';
-import * as L from 'leaflet';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-title = 'our-project';
-streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  detectRetina: true,
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
-wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-  detectRetina: true,
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
-
-layersControl = {
-  baseLayers: {
-    'street Maps': this.streetMaps,
-    'wikimedia Maps': this.wMaps
-  }
-};
-
-options = {
-  layers: [ this.streetMaps],
-  zoom: 7,
-  center: latLng([ 46.879966, -121.726909 ])
-};
-
-drawItems: L.FeatureGroup = L.featureGroup();
-
-drawOptions = {
-position: 'topright',
-edit: {
-  featureGroup: this.drawItems
 }
-};
-
-
-ngOnInit() {
-
-}
-ionViewDidEnter(){
-}
-
-
-onMapReady(map: L.Map) {
-  setTimeout(() => {
-     map.invalidateSize();
-  }, 200);
-}}
